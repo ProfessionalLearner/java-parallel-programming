64717e350e8f82c4b99b85f6a7f0e49a4b55c685
Arnur <arnur.nogaibekov@mail.utoronto.ca>
2020-11-09 20:30:19 +0600

    Commited my solutions for parallel programming course

---
+++ README.md
@@ -0,0 +1,2 @@
+# Parallel programming in Java
+These are my solutions for programming exercises for parallel programming by Rice University. The course was taken on Coursera.
---
+++ miniproject_0/src/main/java/edu/coursera/parallel/Main.java
@@ -0,0 +1,10 @@
+package edu.coursera.parallel;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(12);
+        int sample = Setup.setup(12);
+        System.out.println(sample);
+    }
+
+}
